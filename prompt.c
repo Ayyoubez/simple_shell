f15d79b2edb43443733ae556aaf2a4a19d584127
ayyoubez <ayyoubez@gmail.com>
2023-05-24 02:30:11 -0700

    prompt.c

---
+++ prompt.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+/**
+*handleinput - handle input
+*@str: string
+*@argv: array of strings
+*/
+
+void handleinput(char *str, char *argv[])
+{
+	int i = 0, j = 0;
+
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			str[i] = '\0';
+		i++;
+	}
+	if (str == NULL)
+		return;
+	argv[j] = strtok(str, "");
+	while (argv[j] != NULL)
+	{
+		j++;
+		argv[j] = strtok(NULL, " ");
+	}
+}
+/**
+*runprogram - running commands
+*@argv: array of args
+*@en: envirenment array
+*/
+
+void runprogram(char **argv, char **en)
+{
+	pid_t child;
+
+	child = fork();
+	if (child == -1)
+	{
+		free(argv[0]);
+		exir(EXIT_FAILURE);
+	}
+	if (child == 0)
+	{
+		if (execve(argv[0], argv, en) == -1)
+			perror("Erroe during execution..");
+	}
+	else
+		wait(NULL);
+}
+/**
+*prompt - prompt in terminal
+*@av: argv by main
+*@en: en by main
+*/
+
+void prompt(char **av, char **en)
+{
+	char *str = NULL;
+	ssize_t n_char;
+	size_t n = 0;
+	char *argv[NUM_CMD];
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "Cisfun$", 8);
+		n_char = getline(&str, &n, stdin);
+		if (n_char == -1)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+		handleinput(str, argv);
+		if (!argv[0])
+			continue;
+		builtin(argv[0]);
+		argv[0] = handlepath(_getenv("PATH"), ARGV[0]);
+		if (argv[0] == NULL)
+		{
+			wrie(1, av[0], _strlen(av[0]));
+			write(1, ":No such file or directory\n", 28);
+			continue;
+		}
+		runprogram(argv, en);
+		free(str);
+	}
+}
