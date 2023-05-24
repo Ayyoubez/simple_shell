10ed7d0eb06be11db230bc14e0169113e99b2b06
ayyoubez <ayyoubez@gmail.com>
2023-05-24 01:49:35 -0700

    update

---
+++ builtin.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+*builtin - built in function
+*@cmd: command
+*/
+
+void builtin(char *cmd)
+{
+	int i = 0;
+
+	if (_strcmp(cmd, "exit") == 0)
+		_exit(1);
+	if (_strcmp(cmd, "env") == 0)
+	{
+		while (environ[i])
+		{
+			write(1, environ[i], _strlen(environ[i]));
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+}
